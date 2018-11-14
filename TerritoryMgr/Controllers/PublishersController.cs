cba9839d9f0819ca176feb25ac9e9be80ce6b5c1
Jeff Holman <jeffrey.holman@duke-energy.com>
2018-11-14 11:20:35 -0500

    Renamed to PublishersController

--- TerritoryMgr/Controllers/PublishersController.cs
+++ TerritoryMgr/Controllers/PublishersController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace TerritoryMgr.Controllers
 {
-    public class ValuesController : ApiController
+    public class PublishersController : ApiController
     {
         // GET api/values
         public IEnumerable<string> Get()
